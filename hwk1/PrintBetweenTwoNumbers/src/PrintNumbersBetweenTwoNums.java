d261545a2072d62d59e8175733e0e116dca48323
kaantamerurkun <153518700+kaantamerurkun@users.noreply.github.com>
2024-01-10 16:20:42 +0300

    satisfying requirement change three (#3)

--- hwk1/PrintBetweenTwoNumbers/src/PrintNumbersBetweenTwoNums.java
+++ hwk1/PrintBetweenTwoNumbers/src/PrintNumbersBetweenTwoNums.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class PrintOddNumbersBetweenTwoNums {
+public class PrintEvenNumbersBetweenTwoNums {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -17,18 +18,15 @@ public class PrintOddNumbersBetweenTwoNums {
             System.out.println("Invalid input. The first number should be less than the second number.");
             return;
         }
-
-        // Print odd numbers between a and b
-        printOddNumbersBetweenTwoNums(a, b);
     }
 
-    static void printOddNumbersBetweenTwoNums(int a, int b) {
-        // Print odd numbers between a and b
+    static void printEvenNumbersBetweenTwoNums(int a, int b) {
+        // Print even numbers between a and b
         for (int i = a + 1; i < b; i++) {
-            if (i % 2 != 0) {
-                System.out.print(i + " ");
-            }
-        }
+            if (i % 2 == 0) {
+
+              printEvenNumbersBetweenTwoNums(a, b);
+    }
 
         // Add a new line for better formatting
         System.out.println();
